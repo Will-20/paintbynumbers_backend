528d56c57d20a75a3698c1ed7fba53e7951cfbcb
Will-20 <williamguest42@icloud.com>
2024-09-16 03:39:24 +0100

    Use Redis env

--- index.py
+++ index.py
@@ -1,17 +1,21 @@
 from flask import Flask, request
 from PIL import Image
 from celery.result import AsyncResult
+import os
 
 from tasks import convert
 from utils import celery_init_app
+
+# from dotenv import load_dotenv
+# load_dotenv()
 
 app = Flask(__name__)
 app.config.from_mapping(
     CELERY=dict(
         # broker_url="redis://default:AYDaAAIjcDE2MmE1NjMxMjA2NmU0YTY0YTE5MjM5NWQyMGYzMjA1NnAxMA@normal-ray-32986.upstash.io:6379",
         # result_backend="redis://default:AYDaAAIjcDE2MmE1NjMxMjA2NmU0YTY0YTE5MjM5NWQyMGYzMjA1NnAxMA@normal-ray-32986.upstash.io:6379",
-        broker_url="redis://localhost:6379/0",
-        result_backend="redis://localhost:6379/0",
+        broker_url=f"{os.getenv("REDISCLOUD_URL")}/0",
+        result_backend=f"{os.getenv("REDISCLOUD_URL")}/0",
     )
 )
 celery = celery_init_app(app)
@@ -22,7 +26,6 @@ def task_start():
     image_id = request.form['image_id']
     image_name = request.form['image_name']
     num_colours = request.form['num_colours']
-    print(num_colours)
     mywidth = request.form['width']
 
     task = convert.delay(image_id, image_name, num_colours, mywidth)
